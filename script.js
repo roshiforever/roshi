6c22673d6b95ce2917f407453166ef9d3d6686d7
roshiforever <151558462+roshiforever@users.noreply.github.com>
2023-11-20 22:16:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('drawFlowerBtn').addEventListener('click', function() {
+    // Toggle the visibility of the apology letter container
+    var letterContainer = document.getElementById('apologyLetterContainer');
+    letterContainer.style.display = (letterContainer.style.display === 'none' || letterContainer.style.display === '') ? 'block' : 'none';
+});
+
+document.getElementById('backToMainBtn').addEventListener('click', function() {
+    // Hide the apology letter container when the back button is clicked
+    document.getElementById('apologyLetterContainer').style.display = 'none';
+});
+
+document.getElementById('playAudioBtn').addEventListener('click', function() {
+    var audioPlayer = document.getElementById('audioPlayer');
+
+    // Check if the audio is paused or has ended
+    if (audioPlayer.paused || audioPlayer.ended) {
+        audioPlayer.play();
+    } else {
+        // Pause the audio if it's currently playing
+        audioPlayer.pause();
+    }
+});
+
+// Rest of your existing JavaScript code...

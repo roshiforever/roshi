b58f232aaa3afbb82b4d91bf512457db1d896aac
roshiforever <151558462+roshiforever@users.noreply.github.com>
2023-11-26 18:53:37 +0530

    Add files via upload

--- killme/killme.js
+++ killme/killme.js
@@ -8,18 +8,8 @@ const clickCountElement = document.getElementById("clickCount");
             let clickCount = 0;
 
 const gifs = [
-  "https://media.giphy.com/media/BWVw2C7whuSFCL8ATO/giphy.gif",
-  "https://media.giphy.com/media/aRfmnHSOheokn8914H/giphy.gif",
   "https://media.giphy.com/media/VcKe87r5UxvUzO6lsm/giphy.gif",
-  "https://media.giphy.com/media/3orif0HPtMKPs8su6Q/giphy.gif",
-  "https://media.giphy.com/media/3o6Zt7xWCLZD8f5ZsY/giphy.gif",
-  "https://media.giphy.com/media/3o6Ztl4ZkK5vbgXsmQ/giphy.gif",
-  "https://media.giphy.com/media/3o6ZsSwLqF97shx0pG/giphy.gif",
-  "https://media.giphy.com/media/26gJyLFIxdpupGLJu/giphy.gif",
-  "https://media.giphy.com/media/8D1upwhhOTHo3UKhGy/giphy.gif",
-  "https://media.giphy.com/media/688BJk8U3uzruOgy6Z/giphy.gif",
-  "https://media.giphy.com/media/XbV2eCHp0mHthRjdJP/giphy.gif",
-  "https://media.giphy.com/media/Z5zuypybI5dYc/giphy.gif",
+  "https://media.giphy.com/media/aRfmnHSOheokn8914H/giphy.gif",
   
 ];
 
@@ -28,6 +18,7 @@ yesBtn.addEventListener("click", () => {
   const randomIndex = Math.floor(Math.random() * gifs.length);
   gif.src = gifs[randomIndex];
   question.innerHTML = "RIP RISHIKESH - 2003 TO 2023";
+  
   clickCount++;
                 clickCountElement.textContent = clickCount;
                 if (clickCount === 1) {
